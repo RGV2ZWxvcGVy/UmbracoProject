c0cb7245074bd7a86edf3bbd47637dbf6805c9b2
Marco Stoker <marco@speak.nl>
2024-08-16 14:11:36 +0200

    Added octokit and list of umbraco repositories

---
+++ src/UmbracoProject.Code/Helpers/GitHubHelper.cs
@@ -0,0 +1,12 @@
+﻿using Octokit;
+
+namespace UmbracoProject.Code.Helpers;
+
+public static class GitHubHelper
+{
+    public static IReadOnlyList<Repository> GetRepositories(string organisation)
+    {
+        var client = new GitHubClient(new ProductHeaderValue("my-cool-app"));
+        return client.Repository.GetAllForOrg(organisation).Result;
+    }
+}
